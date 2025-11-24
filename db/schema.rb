291416ad0e22983879cb45496aca5975a38307ff
Victor Dejean <victor.dejean@hotmail.fr>
2025-11-24 17:39:53 +0100

    Generate Plan model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_24_154612) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_24_163632) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "plans", force: :cascade do |t|
+    t.string "title"
+    t.string "theme"
+    t.string "pricing"
+    t.bigint "user_id", null: false
+    t.text "roadmap"
+    t.integer "number_persons"
+    t.string "city"
+    t.string "context"
+    t.integer "lenght_event"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_plans_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +41,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_24_154612) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "plans", "users"
 end
