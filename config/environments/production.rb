f9db5b10d7843cd430377f3d09c0731b32c2e22c
Matteo Garbugli matteo.garbugli@yahoo.it <matteo.garbugli@yahoo.it>
2025-11-24 17:54:53 +0100

    navbar & flashes done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost',  }
+  config.action_mailer.default_url_options = { host: 'event-togo-de3744e9266c.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
