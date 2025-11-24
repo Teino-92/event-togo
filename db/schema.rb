f0dbbde4c6e62af5c518d4cb700624db56cdba6f
Victor Dejean <victor.dejean@hotmail.fr>
2025-11-24 17:44:29 +0100

    Generate chat model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_24_163632) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_24_164354) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chats", force: :cascade do |t|
+    t.string "title"
+    t.bigint "plan_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["plan_id"], name: "index_chats_on_plan_id"
+  end
 
   create_table "plans", force: :cascade do |t|
     t.string "title"
@@ -41,5 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_24_163632) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chats", "plans"
   add_foreign_key "plans", "users"
 end
