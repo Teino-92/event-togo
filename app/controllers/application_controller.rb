5575cdcafe06f545897770c214883474872a6f41
Matteo Garbugli matteo.garbugli@yahoo.it <matteo.garbugli@yahoo.it>
2025-11-25 12:05:22 +0100

    gem LLM install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  #todo before action 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,24 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+
+puts "Creating "
+bob = User.create!(
+  email: "bob@mail.com",
+  password: "secret",
+)
+
+roadmap = <<~ROADMAP
+  TODO: ici on fait comme si c etait l assistant qui avait repondu
+ROADMAP
+
+plan_1 = Plan.create!(
+  title: "",
+  theme: "",
+  pricing: "",
+  user: bob,
+  roadmap: roadmap,
+  number_persons: 4,
+  city: "",
+  context: "",
+  event_lenght: "",
+  roadmap_date: "2025-11-25"
+)
