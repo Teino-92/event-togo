5672e3530ac649f77c5c2d3aa7c64358c3f4b79e
Matteo Garbugli matteo.garbugli@yahoo.it <matteo.garbugli@yahoo.it>
2025-11-26 11:05:50 +0100

    add column to plans

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_24_164540) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_26_100308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_24_164540) do
     t.date "roadmap_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "system_prompt"
     t.index ["user_id"], name: "index_plans_on_user_id"
   end
 
