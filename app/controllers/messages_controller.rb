e1e41098fb8290514adb7982c5a9a1e875fe3d83
Matteo Garbugli matteo.garbugli@yahoo.it <matteo.garbugli@yahoo.it>
2025-11-26 16:42:17 +0100

    chat fixed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
 
   def create
     @chat = current_user.chats.find(params[:chat_id])
-    @challenge = @chat.challenge
+    @plan = @chat.plan
 
     @message = Message.new(message_params)
     @message.chat = @chat
