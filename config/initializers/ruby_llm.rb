2b226e60f36c78f2746fa27c3ad56671255ce745
Matteo Garbugli matteo.garbugli@yahoo.it <matteo.garbugli@yahoo.it>
2025-11-27 16:40:29 +0100

    done

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -3,5 +3,5 @@
 # end
 
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV['OPEN_AI_API_KEY'] # Key for your endpoint
+  config.openai_api_key = ENV['OPENAI_API_KEY'] # Key for your endpoint
 end
